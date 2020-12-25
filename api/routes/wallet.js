8f97c7e58821b67bdc743ca95139c4df4b977736
hb162 <hoangbui1699@gmail.com>
2020-12-25 16:42:21 +0700

    saving commit lan2

--- api/routes/wallet.js
+++ api/routes/wallet.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const walletRouter = express.Router();
 const dailyWalletController = require('../controller/wallet/dailyWallet.js');
+const savingWalletController = require('../controller/wallet/savingWallet.js');
 
-walletRouter.post('/daily/create/', dailyWalletController.createNewWallet);
-
+walletRouter.get('/saving/find/:wallet_id', savingWalletController.getWallet)
+walletRouter.post('/saving/create/', savingWalletController.createNewWallet);
+walletRouter.post('/saving/create/transaction/', savingWalletController.newTransaction)
+walletRouter.get('/saving/:wallet_id', savingWalletController.statisticThisMonth)
 module.exports = walletRouter;
